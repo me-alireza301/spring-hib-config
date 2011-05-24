beee0474ba0c03c166451788d0f781538559125f
javapda <javapda@gmail.com>
2011-05-24 23:42:57 +0000

    add some basic scaffolding

---
+++ spring-hib-configurable/src/main/java/com/gs/sample/component/LegoService.java
@@ -0,0 +1,5 @@
+package com.gs.sample.component;
+
+public interface LegoService {
+
+}
---
+++ spring-hib-configurable/src/main/java/com/gs/sample/component/LegoServiceImpl.java
@@ -0,0 +1,8 @@
+package com.gs.sample.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class LegoServiceImpl implements LegoService {
+
+}
