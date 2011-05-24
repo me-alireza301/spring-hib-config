4f3a9fb6b42feb35716b8601c700a9dbed709cdc
javapda@gmail.com <javapda@gmail.com@ab1fd2e7-c490-0e14-c47d-eaa8f75227b7>
2011-05-24 23:46:24 +0000

    add bean dumper

--- trunk/spring-hib-configurable/src/test/java/com/gs/sample/component/LegoServiceTest.java
+++ trunk/spring-hib-configurable/src/test/java/com/gs/sample/component/LegoServiceTest.java
@@ -1,10 +1,13 @@
 package com.gs.sample.component;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={"classpath:test-lego-config.xml"})
@@ -13,7 +16,17 @@ public class LegoServiceTest {
 	@Autowired
 	private LegoService legoService;
 	
+	@Autowired
+	private ApplicationContext applicationContext;
+	
 	@Test public void basicTesting() {
-		
+		assertNotNull(legoService);
+	}
+	
+	@Ignore
+	@Test public void beanDumper() {
+		for(String s : applicationContext.getBeanDefinitionNames()) {
+			System.out.printf("%s - %s\n",s,applicationContext.getBean(s));
+		}
 	}
 }
