99a262ae44c7850979e977e41a56ac3175b60c83
kdasika@gmail.com <kdasika@gmail.com@ab1fd2e7-c490-0e14-c47d-eaa8f75227b7>
2011-05-24 20:57:04 +0000

    Initial project commit

---
+++ trunk/spring-hib-configurable/src/main/java/com/gs/spring_hib_configurable/App.java
@@ -0,0 +1,13 @@
+package com.gs.spring_hib_configurable;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
